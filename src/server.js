52ec8e380360e3e46dd3e85506fb9c0ea97344cf
Emanuele Bertuol <emanuelebertuol@gmail.com>
2020-08-28 12:05:42 +0200

    .

--- src/server.js
+++ src/server.js
@@ -1,4 +1,40 @@
 const express = require("express")
-const listEndopints = require("express-list-endpoints")
+const listEndPoints = require("express-list-endpoints")
 const cors = require("cors")
 const mongoose = require("mongoose")
+
+const songsRouter = require("./services/songs/index")
+
+const server = express()
+
+const whitelist = ["http://localhost:3002"]
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+
+server.use(cors(corsOptions))
+const port = process.env.PORT
+
+server.use(express.json())
+
+console.log(listEndPoints(server))
+
+mongoose
+  .connect("mongodb+srv://dbTest:dbTest@spotify-auth.beyse.mongodb.net/spotify-auth?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(
+    server.listen(port, () => {
+      console.log("Running on port", port)
+    })
+  )
+  .catch((err) => console.log(err))
